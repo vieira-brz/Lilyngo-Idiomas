b8331749633f176fa3f490c371ab1eb627833c5c
vieira-brz <vieirabraz2003@gmail.com>
2024-04-12 17:25:27 -0300

    feat: Learn Page Wrapper

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -3,7 +3,7 @@ import MobileSidebar from './mobile-sidebar'
 
 const MobileHeader = () => {
   return (
-    <nav className='lg:hidden px-6 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50'>
+    <nav className='lg:hidden px-6 h-[50px] flex items-center bg-indigo-500 border-b fixed top-0 w-full z-50'>
       <MobileSidebar />
     </nav>
   )
