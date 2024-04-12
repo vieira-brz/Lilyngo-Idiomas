836167510b2a6c69f57e91b17171799c3a785abf
vieira-brz <vieirabraz2003@gmail.com>
2024-04-12 16:05:10 -0300

    feat: Footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -14,8 +14,7 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Aprenda, pratique e aprimore novas línguas com Lilyngo
         </h1>
-        <div>
-
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
